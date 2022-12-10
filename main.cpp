291f45fa66c47b671b1675b44543948c4cc874be
Durjoy Ghosh <durjoyghosh716@gmail.com>
2022-12-10 17:05:54 +0600

    added hill

--- main.cpp
+++ main.cpp
@@ -1347,6 +1347,104 @@ glPushMatrix();
 	glVertex2f(-1.f, 1.5f);
 
 	glEnd();
+	glBegin(GL_POLYGON);
+	glColor3ub(224,224,224); // hill 1
+
+	glVertex2f(-1.f, 0.1f);
+	glVertex2f(-0.99f, 0.13f);
+	glVertex2f(-0.98f, 0.15f);
+	glVertex2f(-0.98f, 0.16f);
+	glVertex2f(-0.97f, 0.17f);
+	glVertex2f(-0.97f, 0.18f);
+	glVertex2f(-0.97f, 0.2f);
+	glVertex2f(-0.96f, 0.2f);
+	glVertex2f(-0.96f, 0.19f);
+	glVertex2f(-0.92f, 0.12f);
+	glVertex2f(-0.90f, 0.14f);
+	glVertex2f(-0.90f, 0.2f);
+    glVertex2f(-0.85f, 0.2f);
+	glVertex2f(-0.8f, 0.0f);
+	glVertex2f(-0.78f, -0.04f);
+	glVertex2f(-0.67f, -0.3f);
+    glVertex2f(-0.65f, -0.32f);
+	glVertex2f(-0.65f, -0.31f);
+	glVertex2f(-1.f, -0.3f);
+
+	glEnd();
+
+glBegin(GL_POLYGON);
+	glColor3ub(224,224,224); // hill 2
+
+
+	glVertex2f(-0.74f, -0.3f);
+	glVertex2f(-0.74f, -0.16f);
+	glVertex2f(-0.7f, -0.1f);
+	glVertex2f(-0.67f, -0.1f);
+	glVertex2f(-0.65f, 0.05f);
+	glVertex2f(-0.62f, 0.05f);
+	glVertex2f(-0.56f, 0.21f);
+    glVertex2f(-0.55f, 0.21f);
+	glVertex2f(-0.53f, 0.11f);
+    glVertex2f(-0.52f, 0.11f);
+	glVertex2f(-0.55f, 0.28f);
+	glVertex2f(-0.52f, 0.3f);
+    glVertex2f(-0.55f, 0.48f);
+	glVertex2f(-0.50f, 0.3f);
+    glVertex2f(-0.50f, 0.4f);
+	glVertex2f(-0.40f, 0.17f);
+    glVertex2f(-0.38f, 0.17f);
+	glVertex2f(-0.34f, 0.02f);
+	glVertex2f(-0.338f, 0.02f);
+	glVertex2f(-0.34f, 0.1f);
+	glVertex2f(-0.31f, 0.02f);
+
+	glVertex2f(-0.29f, 0.02f);
+	glVertex2f(-0.27f, -0.03f);
+    glVertex2f(-0.26f, -0.02f);
+	glVertex2f(-0.23f, -0.02f);
+	glVertex2f(-0.20f, -0.10f);
+	glVertex2f(-0.15f, -0.16f);
+	glVertex2f(-0.10f, -0.20f);
+    glVertex2f(-0.05f, -0.25f);
+    glVertex2f(-0.04f, -0.26f);
+    glVertex2f(-0.0f, -0.3f);
+
+	glEnd();
+
+
+
+	glBegin(GL_POLYGON);
+	glColor3ub(224,224,224); // hill 3
+
+    glVertex2f(0.88f, -0.07f);
+    glVertex2f(0.84f, -0.04f);
+    glVertex2f(0.82f, -0.07f);
+    glVertex2f(0.80f, -0.05f);
+    glVertex2f(0.79f, -0.04f);
+    glVertex2f(0.77f, 0.0f);
+    	glVertex2f(0.76f, 0.0f);
+    glVertex2f(0.75f, 0.01f);
+    glVertex2f(0.756f, 0.12f);
+    	glVertex2f(0.74f, 0.15f);
+    glVertex2f(0.66f, -0.06f);
+    glVertex2f(0.62f, -0.02f);
+    	glVertex2f(0.61f, 0.04f);
+    glVertex2f(0.6f, 0.04f);
+    glVertex2f(0.58f, 0.0f);
+    	glVertex2f(0.54f, -0.09f);
+    glVertex2f(0.52f, -0.09f);
+    glVertex2f(0.50f, -0.14f);
+    glVertex2f(0.50f, -0.10f);
+    glVertex2f(0.47f, -0.10f);
+    glVertex2f(0.40f, -0.20f);
+    glVertex2f(0.30f, -0.23f);
+    glVertex2f(0.27f, -0.28f);
+    glVertex2f(0.25f, -0.3f);
+    glVertex2f(0.54f, -0.35f);
+    glVertex2f(1.0f, -0.35f);
+
+
+	glEnd();
 
 
 	//shadow 1
